a3c72ce3587cb36aeca56f1cd0c4951d033ead56
froscoldt <mark.d.denner@gmail.com>
2019-11-07 14:01:14 +0100

    Testing merge conflicts

--- src/main/kotlin/dk/cphbusiness/server/Server.kt
+++ src/main/kotlin/dk/cphbusiness/server/Server.kt
@@ -8,7 +8,7 @@ class WebServer ( val content : WebContent , val port : Int = 80) {
     fun start () { TODO (" Implement ␣ start ") }
     fun stop () { TODO (" Implement ␣ stop ") }
 }
-
+//hej farvel jesper :))))))
 
 
 class Server(val port : Int = 3724) {
