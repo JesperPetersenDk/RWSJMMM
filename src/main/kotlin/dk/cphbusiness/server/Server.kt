6b4c5b881b868fc69df758c71091ce300702b25f
Mads Wulff Nielsen <mwn@Madss-MacBook-Pro-3.local>
2019-11-12 14:06:44 +0100

    reflaction endpoints

--- src/main/kotlin/dk/cphbusiness/server/Server.kt
+++ src/main/kotlin/dk/cphbusiness/server/Server.kt
@@ -3,56 +3,87 @@ package dk.cphbusiness.server
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import java.net.ServerSocket
-import java.net.Socket
-import kotlin.concurrent.thread
 import kotlin.coroutines.CoroutineContext
+import kotlin.reflect.KFunction
+import kotlin.reflect.full.memberExtensionFunctions
+import kotlin.reflect.full.memberFunctions
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.jvm.internal.impl.metadata.ProtoBuf
 
-class WebServer ( val content : IWebContent , val port : Int = 80) {
-    fun start () { TODO (" Implement ␣ start ") }
-    fun stop () { TODO (" Implement ␣ stop ") }
-}
-
-fun main() {
-    Server().serve()
-}
-
-class Server(val port : Int = 3724) : CoroutineScope {
+class WebServer ( val content : ChoirContent , val port : Int) : CoroutineScope  {
     private val job = Job()
     override val coroutineContext : CoroutineContext
         get () = job
 
+    fun getFunctionNames(): MutableMap<String, KFunction<*>> {
+        val type = content::class
+        val map = mutableMapOf<String, KFunction<*>>();
+        for (member in type.memberFunctions) {
+            for (method in Method.values()) {
+                if (member.name.contains(method.name.toLowerCase()))
+                    map.put(member.name.toLowerCase(), member)
+            }
+        }
+        return map
+    }
+
+    fun getFuncName() : MutableList<String> {
+        val type = content::class
+        val list = mutableListOf<String>()
+        for (member in type.memberFunctions) {
+            for (method in Method.values()) {
+                if (member.name.contains(method.name.toLowerCase()))
+                    list.add(member.name)
+            }
+        }
+        return list
+    }
+
     fun handle(request: Request, response: Response) {
         val URL = request.resource
-        if (getTextFromUrl(URL).toLowerCase().contains("moodle")) {
-            response.append("<h1>")
-            response.append("FOR HELVEDE MOODLE :(")
-            response.append("</h1>")
+        val argument = URL.split("/");
+        val endpoint = argument[1]
+        val map = getFunctionNames()
+        val requestedEndpoint = map.get(endpoint)
+
+        if (requestedEndpoint == null) {
+            response.append("Error url")
+            response.send()
+            return
         }
-        println(request.resource)
-        response.append("<h2>")
-        response.append("Hej " + getTextFromUrl(request.resource))
 
-        response.append("</h2>")
-        response.send()
-    }
-
-    fun getTextFromUrl(text : String) : String {
-        return text.split("/")[1]
-    }
-    fun serve() {
-        val serverSocket = ServerSocket(port)
-        while (job.isActive) {
-            val socket = serverSocket.accept()
-            // instead of a thread, use a coroutine
-            launch {
-                handle(Request(socket.getInputStream()), Response(socket.getOutputStream()))
+        when (argument.size) {
+            2 -> {
+                requestedEndpoint.call(content)
+            }
+            3 -> {
+                requestedEndpoint.call(content, Integer.valueOf(argument[2]))
+            }
+            else -> {
+                response.append("Error url")
+                response.send()
             }
         }
 
     }
 
+    fun start () {
+        val serverSocket = ServerSocket(port)
+        while (job.isActive) {
+            val socket = serverSocket.accept()
+            launch {
+                handle(Request(socket.getInputStream()), Response(socket.getOutputStream()))
+            }
 
+        }
+    }
+    fun stop () {
+        job.cancel()
+    }
+}
+
+fun main() {
+    WebServer(ChoirContent(),5000).start()
 }
 
--- src/main/kotlin/dk/cphbusiness/server/WebContent.kt
+++ src/main/kotlin/dk/cphbusiness/server/WebContent.kt
@@ -5,11 +5,14 @@ import java.lang.reflect.Member
 data class Member(val id : Int, val firstName : String, val lastName: String)
 
 class ChoirContent() : IWebContent {
-    fun getMember(): List<Member> =
-        TODO()
-
-    fun getMember(id: Int) : Member? =
-        TODO()
+    fun getMembers(): List<Member> {
+        println("fnadkjgfnsdjlkfmsljdnfg")
+        return mutableListOf()
+    }
+    fun getMember(id: Int) : Member? {
+        println("id: " + id)
+        return null
+    }
 
     fun putMember(member: Member) : Member =
         TODO()
